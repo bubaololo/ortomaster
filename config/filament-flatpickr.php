ba05bff0a5e22185d47a7e0cb6ed6203ec8fef48
bubaololo <bubaololo@gmail.com>
2023-07-01 18:45:48 +0600

    trash removed

--- config/filament-flatpickr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for Savannabits/Flatpickr
-return [
-    'default_theme' => 'default', // 'default','dark','material_blue','material_green','material_red','material_orange','airbnb','confetti'
-];
