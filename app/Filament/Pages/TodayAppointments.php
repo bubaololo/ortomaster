696e25f054b52149086b9ea11a5d3125b5eaea8b
bubaololo <bubaololo@gmail.com>
2023-07-08 04:44:10 +0600

    setters on name

--- app/Filament/Pages/TodayAppointments.php
+++ app/Filament/Pages/TodayAppointments.php
@@ -6,12 +6,9 @@ use App\Filament\Resources\AppointmentResource;
 use App\Filament\Resources\PatientResource;
 use App\Models\Appointment;
 use Carbon\Carbon;
-use Filament\Pages\Page;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Table;
-use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component as Livewire;
 
@@ -55,9 +52,8 @@ class TodayAppointments extends ListRecords
                 TextColumn::make('created_at')
                     ->time()
                     ->label('Время приёма'),
-                TextColumn::make('patient.name')->label('Пациент')
+                TextColumn::make('patient.fullName')->label('Пациент')
                     ->url(fn(Appointment $record) => PatientResource::getUrl('view', ['record' => $record->patient_id])),
-
             ])
             ->actions([
             ])
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,12 +11,65 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Patient extends Model
 {
     use HasFactory;
+    
     protected $guarded = [];
+    protected $fillable = [
+        'name',
+        'surname',
+        'middle_name',
+        'phone',
+        'note',
+        'birthdate',
+        'gender'];
     
     public function appointment(): HasMany
     {
         return $this->hasMany(Appointment::class);
     }
+    
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? ucfirst($value) : null,
+            set: fn(?string $value) => $value ? strtolower($value) : null,
+        );
+    }
+    
+    protected function middleName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? ucfirst($value) : null,
+            set: fn(?string $value) => $value ? strtolower($value) : null,
+        );
+    }
+    
+    protected function surname(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? ucfirst($value) : null,
+            set: fn(?string $value) => $value ? strtolower($value) : null,
+        );
+    }
+    
+    public function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->concatFullName()
+        );
+    }
+    
+    private function concatFullName(): string
+    {
+        return
+            ucwords(
+                str($this->attributes['surname'])
+                    ->append(' ')
+                    ->append($this->attributes['name'])
+                    ->append(' ')
+                    ->append($this->attributes['middle_name'])
+            );
+    }
+    
     
     protected $casts = [
         'diagnosis' => 'array',
