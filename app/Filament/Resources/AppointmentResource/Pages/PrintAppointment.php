f9b275e21271013dbfa381fa4ce571b1c2ca60cd
bubaololo <bubaololo@gmail.com>
2023-07-09 19:37:17 +0600

    blank improved

--- app/Filament/Resources/AppointmentResource/Pages/PrintAppointment.php
+++ app/Filament/Resources/AppointmentResource/Pages/PrintAppointment.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\AppointmentResource;
 use App\Models\Appointment;
 use App\Models\Patient;
 use Carbon\Carbon;
+use Exception;
 use Filament\Resources\Pages\Page;
 
 class PrintAppointment extends Page
@@ -20,7 +21,7 @@ class PrintAppointment extends Page
     {
         
         $this->appointment = Appointment::find($record);
-
+        
         $this->patient = Patient::find($this->appointment->patient_id);
         $this->doctor = auth()->user()->doctor->name;
         $this->birthdate = Carbon::parse($this->patient->birthdate)
@@ -28,9 +29,8 @@ class PrintAppointment extends Page
         $this->date = Carbon::parse($this->appointment->created_at)
             ->translatedFormat('d M Y в h:m');
         $this->age = $this->getAge();
+        $this->checkup = $this->getCheckup();
         
-      
-
         
     }
     
@@ -45,7 +45,27 @@ class PrintAppointment extends Page
         } else {
             $ageString .= 'лет';
         }
-    
+        
         return $ageString;
+    }
+    
+    private function getCheckup()
+    {
+        $checkupPeriod = $this->appointment->checkup_date;
+        try {
+            if (str_contains($checkupPeriod, '3')) {
+                $finalDate = Carbon::parse($this->appointment->created_at)->addMonths(3)->translatedFormat('d M');
+            } elseif (str_contains($checkupPeriod, '6')) {
+                $finalDate = Carbon::parse($this->appointment->created_at)->addMonths(6)->translatedFormat('d M');
+            } elseif (str_contains($checkupPeriod, '1')) {
+                $finalDate = Carbon::parse($this->appointment->created_at)->addYear()->translatedFormat('d M');
+            } else {
+                throw new Exception('Неверное значение даты обследования');
+            }
+        } catch (Exception $e) {
+            echo 'Ошибка: ' . $e->getMessage();
+        }
+        
+        return $checkupPeriod.' - '.$finalDate;
     }
 }
--- resources/views/filament/resources/appointment-resource/pages/print-appointment.blade.php
+++ resources/views/filament/resources/appointment-resource/pages/print-appointment.blade.php
@@ -1,235 +1,170 @@
 <x-filament::page>
-    <div class="print">
-        <style>
-            /*@media print {*/
-            /*    !*aside, header, .filament-main-footer {*!*/
-            /*    !*    display: none;*!*/
-            /*    !*}*!*/
-            /*    body {*/
-            /*        visibility: hidden;*/
 
-            /*    }*/
-            /*    header {*/
-            /*        display: none;*/
-            /*    }*/
-            /*    .print {*/
-            /*        visibility: visible;*/
-            /*        position: fixed;*/
-            /*        top: 5px;*/
-            /*        left: 5px;*/
-            /*    }*/
-            /*}*/
-            .logo {
-                width: 30vw;
-                margin: 0 auto;
-            }
+    <main class="print">
 
-            .main-title {
-                margin-top: 20pt;
-                font-size: 25pt;
-                text-align: center;
-            }
+        <link rel="stylesheet" href="{{ asset('css/print-page.css') }}">
+        <div class="print__inner">
+            <section class="header">
+                <img src="{{ asset('/img/logo.svg') }}" alt="Logo" class="logo">
+                <div class="main-title">Протокол плантоскопии</div>
+            </section>
+            <section class="table">
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        {{--<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">--}}
+                        {{--<tr>--}}
+                        {{--    <th scope="col" class="px-6 py-3">--}}
+                        {{--        Имя--}}
+                        {{--    </th>--}}
+                        {{--    <th scope="col" class="px-6 py-3">--}}
+                        {{--        Диагноз--}}
+                        {{--    </th>--}}
+                        {{--</tr>--}}
+                        {{--</thead>--}}
+                        <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Фио
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $patient->fullName  }}
+                            </td>
 
-            table {
-                font-size: 10pt !important;
-                line-height: 15pt !important;
-            }
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Дата рождения
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $this->birthdate }} ({{ $this->age }})
+                            </td>
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Дата и время обследования
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $this->date }}
+                            </td>
+                        </tr>
+                        @if($appointment->diabetic)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <strong>Индивидуальные особенности:</strong>
+                                </th>
+                                <td class="px-6 py-4">
+                                    <strong style="text-decoration: underline">ДИАБЕТИЧЕСКАЯ СТОПА</strong>
+                                </td>
+                            </tr>
+                        @endif
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Диагноз
+                            </th>
+                            <td class="px-6 py-4">
+                                @foreach($appointment->diagnosis as $diag)
+                                    @if(!$loop->last)
+                                        {{ $diag }},
+                                    @else
+                                        {{ $diag }}
+                                    @endif
+                                @endforeach
+                            </td>
+                        </tr>
 
-            .rec {
-                margin-top: 40pt;
-                font-size: 8pt !important;
-                line-height: 12pt !important;
-                position: relative;
-            }
-
-            .qr-code {
-                position: absolute;
-                top: 0;
-                right: 0;
-                text-align: center;
-            }
-
-            .rec__title {
-                font-weight: bold;
-                margin-bottom: 20pt;
-            }
-
-            .rec__item {
-                margin-bottom: 4pt;
-            }
-
-            .arc {
-                display: flex;
-                gap: 10pt;
-            }
-
-            .doc {
-                font-style: italic;
-                text-align: right;
-            }
-
-            .subs {
-                margin-top: 40pt;
-                font-size: 10pt;
-                font-style: italic;
-            }
-        </style>
-        <img src="{{ asset('/img/logo.svg') }}" alt="Logo" class="logo">
-        {{--<img src="{{ asset( 'storage/'.$appointment->photo) }}" alt="">--}}
-        <div class="main-title">Протокол плантоскопии</div>
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                {{--<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">--}}
-                {{--<tr>--}}
-                {{--    <th scope="col" class="px-6 py-3">--}}
-                {{--        Имя--}}
-                {{--    </th>--}}
-                {{--    <th scope="col" class="px-6 py-3">--}}
-                {{--        Диагноз--}}
-                {{--    </th>--}}
-                {{--    <th scope="col" class="px-6 py-3">--}}
-                {{--        Дата рождения--}}
-                {{--    </th>--}}
-
-                {{--</tr>--}}
-                {{--</thead>--}}
-                <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Фио
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ $patient->fullName  }}
-                    </td>
-
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Дата рождения
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ $this->birthdate }} ({{ $this->age }})
-                    </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Дата и время обследования
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ $this->date }}
-                    </td>
-                </tr>
-                @if($appointment->diabetic)
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <strong>Индивидуальные особенности:</strong>
-                        </th>
-                        <td class="px-6 py-4">
-                            <strong style="text-decoration: underline">ДИАБЕТИЧЕСКАЯ СТОПА</strong>
-                        </td>
-                    </tr>
-                @endif
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Диагноз
-                    </th>
-                    <td class="px-6 py-4">
-                        @foreach($appointment->diagnosis as $diag)
-                            @if(!$loop->last)
-                                {{ $diag }},
-                            @else
-                                {{ $diag }}
-                            @endif
-                        @endforeach
-                    </td>
-                </tr>
-
-                </tbody>
-            </table>
-        </div>
-        <div class="rec">
-            <div class="rec__title">
-                Рекомендации:
-            </div>
-            <div class="rec__item">
-                ЛФК
-            </div>
-            <div class="rec__item">
-                Балансировочный диск 10-15 мин. в день
-            </div>
-            <div class="rec__item">
-                Ортопедический коврик
-            </div>
-            <div class="rec__item">
-                Ходьба по зыбучей поверхности (песок)
-            </div>
-            <div class="rec__item">
-                Подушка-балансир по 5-10 мин. 2-3 р\день (диск-балансир)
-            </div>
-            <div class="rec__item">
-                <strong>Тейпирование для тела</strong>
-            </div>
-
-            @if ($appointment->bus)
-                <div class="rec__item">
-                    <strong>Отводящая шина:</strong> {{ $appointment->bus  }}
+                        </tbody>
+                    </table>
                 </div>
-            @endif
-            @if ($appointment->shoes)
-                {{--SHOES--}}
-                <div class="rec__item">
-                    <strong>Обувь:</strong> {{ $appointment->shoes }}
-                </div>
-                @if ($appointment->shoes_sides)
-                <div class="rec__item">
-                    <strong>Укрепление борта:</strong> {{ $appointment->shoes_sides }}
-                </div>
-                @endif
-                {{--!SHOES--}}
-            @else
-                {{--INSOLES--}}
-                <div class="rec__item">
-                    <strong>Ортопедические стельки:</strong>
-                </div>
-                <div class="rec__item arc">
-                    <div class="arc__title"><strong>Высота сводов:</strong></div>
-                    <div class="arc__grid">
-                        <div class="arc__top-row">
-                            продольный свод: слева <strong>{{ $appointment->longitudinal_arch_left }}</strong>, справа
-                            <strong>{{ $appointment->longitudinal_arch_right }}</strong>
+            </section>
+            <section class="content">
+                <div class="rec">
+                    <div class="rec__title">
+                        Рекомендации:
+                    </div>
+                    <div class="rec__item">
+                        ЛФК; Балансировочный диск 10-15 мин. в день; Ортопедический коврик; Ходьба по зыбучей поверхности (песок);
+                        Подушка-балансир по 5-10 мин. 2-3 р\день (диск-балансир); <strong>Тейпирование для тела</strong>
+                    </div>
+                    {{--<div class="rec__item">--}}
+                    {{--    Балансировочный диск 10-15 мин. в день--}}
+                    {{--</div>--}}
+                    {{--<div class="rec__item">--}}
+                    {{--    Ортопедический коврик--}}
+                    {{--</div>--}}
+                    {{--<div class="rec__item">--}}
+                    {{--    Ходьба по зыбучей поверхности (песок)--}}
+                    {{--</div>--}}
+                    {{--<div class="rec__item">--}}
+                    {{--    Подушка-балансир по 5-10 мин. 2-3 р\день (диск-балансир)--}}
+                    {{--</div>--}}
+                    {{--<div class="rec__item">--}}
+                    {{--    <strong>Тейпирование для тела</strong>--}}
+                    {{--</div>--}}
+
+                    @if ($appointment->bus)
+                        <div class="rec__item">
+                            <strong>Отводящая шина:</strong> {{ $appointment->bus  }}
                         </div>
-                        <div class="arc__bot-row">
-                            поперечный свод: <strong>{{ $appointment->transverse_arch  }}</strong>
+                    @endif
+                    @if ($appointment->shoes)
+                        {{--SHOES--}}
+                        <div class="rec__item">
+                            <strong>Обувь:</strong> {{ $appointment->shoes }}
                         </div>
+                        @if ($appointment->shoes_sides)
+                            <div class="rec__item">
+                                <strong>Укрепление борта:</strong> {{ $appointment->shoes_sides }}
+                            </div>
+                        @endif
+                        {{--!SHOES--}}
+                    @else
+                        {{--INSOLES--}}
+                        <div class="rec__item">
+                            <strong>Ортопедические стельки:</strong>
+                        </div>
+                        <div class="rec__item arc">
+                            <div class="arc__title"><strong>Высота сводов:</strong></div>
+                            <div class="arc__grid">
+                                <div class="arc__top-row">
+                                    продольный свод: слева <strong>{{ $appointment->longitudinal_arch_left }}</strong>, справа
+                                    <strong>{{ $appointment->longitudinal_arch_right }}</strong>
+                                </div>
+                                <div class="arc__bot-row">
+                                    поперечный свод: <strong>{{ $appointment->transverse_arch  }}</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="rec__item">
+                            <strong>Пронаторы:</strong> {{ $appointment->pronator_type }} слева: <strong>{{ $appointment->pronator_left }}</strong>, справа:
+                            <strong>{{ $appointment->pronator_right }}</strong>
+                        </div>
+                        <div class="rec__item">
+                            <strong>Удобная, широкая обувь с высотой каблука не более 3.0 см</strong>
+                        </div>
+                        {{--!INSOLES--}}
+                    @endif
+
+                    <div class="qr-code">
+                        {!! QrCode::size(130)->generate(asset( 'storage/'.$appointment->photo)); !!}
+                        Фото
                     </div>
                 </div>
-                <div class="rec__item">
-                    <strong>Пронаторы:</strong> {{ $appointment->pronator_type }} слева: <strong>{{ $appointment->pronator_left }}</strong>, справа:
-                    <strong>{{ $appointment->pronator_right }}</strong>
+                <div class="appointment">
+                    Повторный приём через: {{ $this->checkup  }}&#42
                 </div>
-                <div class="rec__item">
-                    <strong>Удобная, широкая обувь с высотой каблука не более 3.0 см</strong>
+            </section>
+            <section class="footer">
+                <div class="doc">
+                    Вр. {{ $doctor }}
                 </div>
-                {{--!INSOLES--}}
-            @endif
-
-            <div class="qr-code">
-                {!! QrCode::size(130)->generate(asset( 'storage/'.$appointment->photo)); !!}
-                Фото
-            </div>
+                <div class="subs">
+                    *Скидка на повторный приём врача-ортопеда в течение 6 месяцев -50%.<br>
+                    *Скидка действует ТОЛЬКО ПРИ ПРЕДЪЯВЛЕНИИ ПРОТОКОЛА О ПРОХОЖДЕНИИ ПЛАНТОСКОПИИ в филиалах " <strong>Orto-Master</strong>".<br>
+                    *При одновременном заказе второй пары ортопедических стелек действует скидка 20%.
+                </div>
+            </section>
         </div>
-
-        <div class="footer">
-            <div class="doc">
-                Вр. {{ $doctor }}
-            </div>
-            <div class="subs">
-                *Скидка на повторный приём врача-ортопеда в течение 6 месяцев -50%.<br>
-                *Скидка действует ТОЛЬКО ПРИ ПРЕДЪЯВЛЕНИИ ПРОТОКОЛА О ПРОХОЖДЕНИИ ПЛАНТОСКОПИИ в филиалах " <strong>Orto-Master</strong>".<br>
-                *При одновременном заказе второй пары ортопедических стелек действует скидка 20%.
-            </div>
-        </div>
-    </div>
+    </main>
     <div class="print-button filament-button filament-button-size-md inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action cursor-pointer">
         <svg class="filament-button-icon w-5 h-5 mr-1 -ml-2 rtl:ml-1 rtl:-mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"></path>
